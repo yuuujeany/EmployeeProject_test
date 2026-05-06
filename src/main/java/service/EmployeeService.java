e1b27618d8cf537be9b2ee1c6d83516c545605f3
pyouji99@naver.com <parkbori99@gmail.com>
2026-05-06 13:26:02 +0900

    feat : 기본 컨트롤러 인터페이스 생성

--- src/main/java/service/EmployeeService.java
+++ src/main/java/service/EmployeeService.java
@@ -1,9 +1,37 @@
 package service;
 
+import vo.EmployeeVo;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class EmployeeService {
     private static EmployeeService instance = new EmployeeService();
+    private ArrayList<EmployeeVo> list;
+    private  EmployeeService(){
+        list = new ArrayList<EmployeeVo>();
+        // 파일 로드해서 리스트에 초기화
+        loadToCSV();
 
-    private  EmployeeService(){}
+    }
+
+    private void loadToCSV() {
+        try(FileReader fr = new FileReader( "employee.csv");
+            BufferedReader br = new BufferedReader(fr)){
+
+            while (true){
+                String str = br.readLine();
+                if(str == null) break;
+                System.out.println(str);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static EmployeeService getInstance(){
         if(instance == null)
