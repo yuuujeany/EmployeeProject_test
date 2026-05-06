ed6a21887b852857069e5f1a1715dd63166b3ed7
pyouji99@naver.com <parkbori99@gmail.com>
2026-05-06 14:54:32 +0900

    feat : 사원 정보 추가 기능 작업완료

--- src/main/java/service/EmployeeService.java
+++ src/main/java/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package service;
 
-import vo.EmployeeVo;
+import exception.EmployeeException;
+import vo.EmployeeVO;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -10,42 +11,52 @@ import java.util.ArrayList;
 
 public class EmployeeService {
     private static EmployeeService instance = new EmployeeService();
-    private ArrayList<EmployeeVo> list;
-    private  EmployeeService(){
-        list = new ArrayList<EmployeeVo>();
-        // 파일 로드해서 리스트에 초기화
+    private ArrayList<EmployeeVO> list;
+    private EmployeeService() {
+        list = new ArrayList<>();
+        //파일 로드해서 리스트에 초기화
         loadToCSV();
-
     }
 
-    public ArrayList<EmployeeVo> getList() {
+    public ArrayList<EmployeeVO> getList() {
         return list;
     }
 
     private void loadToCSV() {
-        try(FileReader fr = new FileReader( "employee.csv");
+        try(FileReader fr = new FileReader("employee.csv");
             BufferedReader br = new BufferedReader(fr)){
-            br.readLine();//필드 목록 건너뛰기
-            while (true){
+            br.readLine();//필드 목록 건너뜀
+            while(true){
                 String str = br.readLine();
                 if(str == null) break;
 //                System.out.println(str);
                 String[] arr = str.split(",");
-                list.add(new EmployeeVo(arr[0],arr[1],arr[2],
+                list.add(new EmployeeVO(arr[0],arr[1],arr[2],
                         Integer.parseInt(arr[3]),arr[4]));
             }
             //파일 로드 완료
             System.out.println("사원 정보 로드 완료");
-
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static EmployeeService getInstance(){
-        if(instance == null)
+    public static EmployeeService getInstance() {
+        if (instance == null)
             instance = new EmployeeService();
-        return  instance;
+        return instance;
+    }
+
+    public void checkDuplicateEmployeeId(String id) throws EmployeeException {
+
+        int idx = list.indexOf(new EmployeeVO(id,null,null,0,null));
+
+        if(idx != -1)
+            throw new EmployeeException("사원번호가 중복되었습니다.");
+
+    }
+
+    public boolean appendEmployee(EmployeeVO employeeVO) {
+        return list.add(employeeVO);
     }
 }
