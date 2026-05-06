a25c728568b982da1f03f0aa72de971527fc93d4
pyouji99@naver.com <parkbori99@gmail.com>
2026-05-06 11:27:47 +0900

    feat : Service 클래스,VO 클래스 작성

---
+++ src/main/java/vo/EmployeeVo.java
@@ -0,0 +1,82 @@
+package vo;
+
+import java.util.Objects;
+
+public class EmployeeVo {
+    private String id;
+    private String name;
+    private String position;
+    private int salary;
+    private String hireDate;
+
+    public EmployeeVo(String id, String name, String position, int salary, String hireDate) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+        this.hireDate = hireDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeVo{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", salary=" + salary +
+                ", hireDate='" + hireDate + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        EmployeeVo that = (EmployeeVo) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
