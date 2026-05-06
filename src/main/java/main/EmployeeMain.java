7dbd0afff6114f87c4169a6817b984e5d8f1d1e0
pyouji99@naver.com <parkbori99@gmail.com>
2026-05-06 11:46:06 +0900

    feat : Main 클래스 작성

---
+++ src/main/java/main/EmployeeMain.java
@@ -0,0 +1,36 @@
+package main;
+
+
+import controller.Controller;
+import controller.HandlerMapping;
+
+import java.util.Scanner;
+
+public class EmployeeMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("-- 사원 관리 프로그램 --");
+            System.out.println ("1. 사원 정보 등록");
+            System.out.println("2. 사원 정보 삭제");
+            System.out.println("3. 사원 정보 수정");
+            System.out.println("4. 사원 정보 검색 (사번)");
+            System.out.println("5. 전체 사원 조회");
+            System.out.println("0. 프로그램 종료");
+            System.out.println("원하시는 메뉴번호를 입력하세요 : ");
+            int no =sc.nextInt();sc.nextLine();
+
+            Controller controller = HandlerMapping.getInstance().createController(no);
+
+            if (controller !=null)
+                controller.execute(sc);
+
+            if(no == 0){
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+        }
+    }
+}
